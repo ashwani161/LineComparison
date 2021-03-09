b7ebe7f7b80a125f907332abd0974b9b77e46a59
ashwani161 <ashwanin65@gmail.com>
2021-03-09 18:00:06 +0530

    Added:Check lengths are equal or not

--- src1/com/line/line.java
+++ src1/com/line/line.java
@@ -1,5 +1,6 @@
 package com.line;
 import java.util.Scanner;
+import java.util.Arrays;
 public class line {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Line");
@@ -19,8 +20,46 @@ public class line {
 		x2 = scan.nextInt();
 		System.out.println("Please enter Y2 coordinate: ");
 		y2 = scan.nextInt();
-		scan.close();
 		length = Math.sqrt(Math.pow(x2-x1, 2) + (Math.pow(y2-y1, 2)));
 		System.out.println("The Length of line is" + (length));
+		
+		double r1 = 0;
+		double s1 = 0;
+		double r2 = 0;
+		double s2 = 0;
+		System.out.println("Please enter R1 coordinate: ");
+		x1 = scan.nextInt();
+		System.out.println("Please enter S1 coordinate: ");
+		y1 = scan.nextInt();
+		System.out.println("Please enter R2 coordinate: ");
+		x2 = scan.nextInt();
+		System.out.println("Please enter S2 coordinate: ");
+		y2 = scan.nextInt();
+		scan.close();
+		double a1[]= {x1,y1,x2,y2};
+		double a2[]= {r1,s1,r2,s2};
+		boolean status=true;
+		if(a1.length == a2.length)
+		{
+			for(int i=0;i<a1.length;i++)
+			{
+				if(a1[i] !=a2[i])
+				{
+					status=false;
+				}
+			}
+		}
+		else
+		{
+			status=false;
+		}
+		if(status==true)
+		{
+			System.out.println("lengths are Equal");
+		}
+		else
+		{
+			System.out.println("lengths are NOT equal");
+		}
 	}
 }
