133f618607e0023bfbcd2bae6687950993428f4d
ashwani161 <ashwanin65@gmail.com>
2021-03-09 13:53:43 +0530

    Refactor:Welcome to Line

---
+++ src1/com/line/line.java
@@ -0,0 +1,10 @@
+package com.line;
+
+public class line {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Line");
+		System.out.println("Comparison Computation");
+		System.out.println("Program on Master Branch");
+		
+	}
+}
