67dfd389dd1509dc5f2404310ad77b4ed5ddf25a
ashwani161 <ashwanin65@gmail.com>
2021-03-09 12:36:05 +0530

    Added:Welcome to Line

---
+++ src/Line.java
@@ -0,0 +1,8 @@
+
+public class Line {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line");
+		System.out.println("Comparison Computation");
+		System.out.println("Program on Master Branch");
+	}
+}
